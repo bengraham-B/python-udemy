92ffff9844aa5fcee70e8c2076262c93fc8ac8ed
Ben Graham <grahamben7@gmail.com>
2024-02-27 10:08:23 +0200

    day8

---
+++ day8/greet.py
@@ -0,0 +1,6 @@
+def greet(name, name1, name2):
+    print("Hello", name)
+    print("Howzit", name1)
+    print("Sup", name2)
+
+greet(name="Toby", name1="Max", name2="Bella")
---
+++ day8/info.py
@@ -0,0 +1,4 @@
+def happy():
+    print("Hello Ben")
+
+happy()
