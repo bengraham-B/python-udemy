426c90a28e5075f2d1b7b804f5f2c3775306e69b
Ben Graham <grahamben7@gmail.com>
2024-02-28 20:15:48 +0200

    Pull

--- day8/test.py
+++ day8/test.py
@@ -1,1 +1,6 @@
-print(45 % 26)
+print(12 % 26)
+print(3 % 26)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(alphabet[3%26])
